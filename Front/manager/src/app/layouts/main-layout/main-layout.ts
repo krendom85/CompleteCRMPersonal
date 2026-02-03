e8a4803ef90b04f589efd319afa3540c6d471749
Christian Moreno <95726720+krendom85@users.noreply.github.com>
2026-02-02 23:27:11 -0500

    Se realiza implementaicones para la nevegacion del proyecto

--- Front/manager/src/app/layouts/main-layout/main-layout.ts
+++ Front/manager/src/app/layouts/main-layout/main-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../shared/components/navbar/navbar";
 import { RouterOutlet } from "@angular/router";
+import { Topbar } from "../../shared/components/topbar/topbar";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [Navbar, RouterOutlet],
+  imports: [Navbar, RouterOutlet, Topbar],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
